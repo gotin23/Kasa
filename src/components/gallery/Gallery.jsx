4c9b0dcbc039aa19d2c397532efd5ae181d75dc0
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-05-09 15:42:28 +0200

    ajout d'un compteur pour la gallerie

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -8,18 +8,14 @@ export default function Gallery({ state }) {
   console.log(state.pics[0]);
   const [activeSlide, setActiveSlide] = useState(0);
   const changeSlide = (value, e) => {
-    console.log(e);
     const nbSlide = state.pics.length;
     const rightArrow = e.target.classList.contains("right-arrow");
     const leftArrow = e.target.classList.contains("left-arrow");
-    console.log(nbSlide);
-    if (leftArrow && activeSlide === 0) {
-      console.log("debut");
 
+    if (leftArrow && activeSlide === 0) {
       return setActiveSlide(nbSlide - 1);
     }
     if (rightArrow && activeSlide === nbSlide - 1) {
-      console.log("fin");
       return setActiveSlide(0);
     } else {
       setActiveSlide(activeSlide + value);
@@ -30,6 +26,7 @@ export default function Gallery({ state }) {
       <img src={state.pics[activeSlide]} alt={"photo de " + state.alt} className="active-pic" />
       <img src={LeftArrow} alt="fleche gauche" className="left-arrow" onClick={(e) => changeSlide(-1, e)} />
       <img src={RightArrow} alt="fleche droite" className="right-arrow" onClick={(e) => changeSlide(1, e)} />
+      <p className="">{state.pics.length > 1 && activeSlide + 1 + "/" + state.pics.length}</p>
     </div>
   );
 }
